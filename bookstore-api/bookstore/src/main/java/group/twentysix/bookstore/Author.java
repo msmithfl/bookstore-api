cf2c352f2572d7d3f243ef1e956736e839b2ec8e
wazza1295 <waseempid@outlook.com>
2023-03-28 15:41:12 -0400

    Create Author.java
    Created Author class with a constructor that allows admin to create a new author.

---
+++ bookstore-api/bookstore/src/main/java/group/twentysix/bookstore/Author.java
@@ -0,0 +1,20 @@
+package group.twentysix.bookstore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.List;
+@Document(collection = "Employee")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Author {
+    @Id
+    private ObjectId id;
+    private String firstName;
+    private String lastName;
+    private String biography;
+    private String publisher;
+}
